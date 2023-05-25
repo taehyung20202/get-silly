5156fa064d977e4dc886bcaa7ea4e20ef1ee99ab
taehyung20202 <134577098+taehyung20202@users.noreply.github.com>
2023-05-25 14:45:44 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showLeds(`
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
